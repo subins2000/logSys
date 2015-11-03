9c1954159f45ea0c716e6c5a8e16b075b9fa0a5c
Subin Siby <subins2000@gmail.com>
2015-11-03 20:33:05 +0530

    Version 0.5
    What's New :
    * Two Step Login
    * Fixed Bugs
    * Manage Devices
    * More Pages
    * Improved Examples

--- class.logsys.php
+++ class.logsys.php
@@ -22,7 +22,7 @@ namespace Fr;
 /**
 .---------------------------------------------------------------------------.
 |  Software: PHP Login System - PHP logSys                                  |
-|  Version: 0.4 (Last Updated on 2015 September 1)                          |
+|  Version: 0.5 (Last Updated on 2015 November 3)                           |
 |  Contact: http://github.com/subins2000/logsys                             |
 |  Documentation: https://subinsb.com/php-logsys                            |
 |  Support: http://subinsb.com/ask/php-logsys                               |
