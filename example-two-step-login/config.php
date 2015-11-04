00c143b1200562a44b24a7607937eaf7c31a41c0
Subin Siby <subins2000@gmail.com>
2015-11-04 16:50:20 +0530

    Added extra config values

--- example-two-step-login/config.php
+++ example-two-step-login/config.php
@@ -1,6 +1,10 @@
 <?php
 require __DIR__ . "/../class.logsys.php";
 $config = array(
+  "basic" => array(
+    "company" => "My Site",
+    "email" => "emails@mysite.com"
+  ),
   "db" => array(
     "host" => "localhost",
     "port" => 3306,
@@ -25,7 +29,8 @@ $config = array(
   "two_step_login" => array(
     "instruction" => "A token was sent to your E-Mail Address. Please see the mail in your inbox and paste the token found in the textbox below :",
     "send_callback" => function($userID, $token){
-      echo $token;
+      $email = \Fr\LS::getUser("email", $userID);
+      \Fr\LS::sendMail($email, "Verify Yourself", "Someone tried to login to your account. If it was you, then use the following token to complete logging in : <blockquote>". $token ."</blockquote>If it was not you, then ignore this email and please consider to change your account's password.");
     }
   )
 );
