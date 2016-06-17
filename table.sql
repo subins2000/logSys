6ba398ec92c43c7acac5ed41a5050e755226b6fa
Subin Siby <subins2000@gmail.com>
2016-06-17 08:20:23 +0530

    Upgrade table for version 0.6

--- table.sql
+++ table.sql
@@ -15,8 +15,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`username` varchar(10) NOT NULL,
 	`email` tinytext NOT NULL,
-	`password` varchar(64) NOT NULL,
-	`password_salt` varchar(20) NOT NULL,
+	`password` varchar(255) NOT NULL,
 	`name` varchar(30) NOT NULL,
 	`created` datetime NOT NULL,
 	`attempt` varchar(15) NOT NULL DEFAULT '0',
@@ -44,19 +43,3 @@ CREATE TABLE IF NOT EXISTS `user_devices` (
   `last_access` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-
---
--- Table structure for table `user_yubikeys`
---
-
-CREATE TABLE IF NOT EXISTS `user_yubikeys` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` int(11) NOT NULL COMMENT 'The user''s ID',
-  `keyid` varchar(12) NOT NULL COMMENT 'Yubikey Unique Identifier',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-
-
-
