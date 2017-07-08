b77cde0aedee54400b3f20daef853dd409d1fcca
Subin Siby <subins2000@gmail.com>
2017-07-09 01:41:44 +0530

    Use IP address if server name is null

--- src/Fr/LS.php
+++ src/Fr/LS.php
@@ -1504,12 +1504,18 @@ HTML;
             $pageURL .= 's';
         }
 
+        if ($_SERVER['SERVER_NAME'] == null) {
+            $serverName = $_SERVER['SERVER_ADDR'];
+        } else {
+            $serverName = $_SERVER['SERVER_NAME'];
+        }
+
         $pageURL .= '://';
 
         if ($_SERVER['SERVER_PORT'] !== '80') {
-            $pageURL .= $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . $_SERVER['REQUEST_URI'];
+            $pageURL .= $serverName . ':' . $_SERVER['SERVER_PORT'] . $_SERVER['REQUEST_URI'];
         } else {
-            $pageURL .= $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+            $pageURL .= $serverName . $_SERVER['REQUEST_URI'];
         }
 
         return $pageURL;
